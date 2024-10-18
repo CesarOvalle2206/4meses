7f6b029c4e9d125ede8cc5e7118eec49694663b7
CesarOvalle2206 <cesarovalle2206@gmail.com>
2024-10-18 15:08:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function loguear(){
+
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;
+
+if(username=="AndreinaRomero" && password=="18-06-2024")
+{
+    
+    window.location="mivida.html";
+}
+else
+{
+    alert("Datos Incorrectos Impostor");
+}
+
+}
